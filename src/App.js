95c498a0c582e7e1699cdffb53b4fd08973b8184
Maciej Szurlej <maciekszurlej@wp.pl>
2021-10-12 21:18:27 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "weather-icons/css/weather-icons.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Weather from "./components/weather.components";
+import Form from "./components/form.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_key = "051fe9e8f3bf1e3a65cdcfda0281e9af";
+/** api.openweathermap.org/data/2.5/weather?q=London,uk&appid={API key} */
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      city: undefined,
+      country: undefined,
+      icon: undefined,
+      main: undefined,
+      celsius: undefined,
+      temp_max: undefined,
+      temp_min: undefined,
+      description: "",
+      error: false,
+    };
+
+    this.weathericon = {
+      Thunderstorm: "wi-thunderstorm",
+      Drizzle: "wi-sleet",
+      Rain: "wi-storm-showers",
+      Snow: "wi-snow",
+      Atmosphere: "wi-fog",
+      Clear: "wi-day-sunny",
+      Clouds: "wi-day-fog",
+    };
+  }
+
+  calCelsius(temp) {
+    let cell = Math.floor(temp - 273.15);
+    return cell;
+  }
+
+  get_Weathericon(icons, rangeID) {
+    switch (true) {
+      case rangeID >= 200 && rangeID <= 232:
+        this.setState({ icon: this.weathericon.Thunderstorm });
+        break;
+      case rangeID >= 300 && rangeID <= 321:
+        this.setState({ icon: this.weathericon.Drizzle });
+        break;
+      case rangeID >= 500 && rangeID <= 531:
+        this.setState({ icon: this.weathericon.Rain });
+        break;
+      case rangeID >= 600 && rangeID <= 622:
+        this.setState({ icon: this.weathericon.Snow });
+        break;
+      case rangeID >= 701 && rangeID <= 781:
+        this.setState({ icon: this.weathericon.Atmosphere });
+        break;
+      case rangeID === 800:
+        this.setState({ icon: this.weathericon.Clear });
+        break;
+      case rangeID >= 801 && rangeID <= 804:
+        this.setState({ icon: this.weathericon.Clouds });
+        break;
+      default:
+        this.setState({ icon: this.weathericon.Clouds });
+    }
+  }
+
+  getWeather = async (e) => {
+    e.preventDefault();
+
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+    if (city && country) {
+      const api_call = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`
+      );
+      const response = await api_call.json();
+
+      console.log(response);
+
+      this.setState({
+        city: `${response.name}, ${response.sys.country}`,
+        celsius: this.calCelsius(response.main.temp),
+        temp_max: this.calCelsius(response.main.temp_max),
+        temp_min: this.calCelsius(response.main.temp_min),
+        description: response.weather[0].description,
+      });
+      this.get_Weathericon(this.weathericon, response.weather[0].id);
+    } else {
+      this.setState({ error: true });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="App">
+          <Form loadweather={this.getWeather} error={this.state.error} />
+          <Weather
+            city={this.state.city}
+            country={this.state.country}
+            temp_celsius={this.state.celsius}
+            temp_max={this.state.temp_max}
+            temp_min={this.state.temp_min}
+            description={this.state.description}
+            weathericon={this.state.icon}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
